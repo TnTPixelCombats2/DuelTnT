b8b9a0c572493ecd50e9e49f2ee8dbfe474354ea
TnTPixelCombats2 <tratiliwanilotnt21@gmail.com>
2025-04-27 11:48:26 +0700

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -4,8 +4,8 @@ import { DisplayValueHeader } from 'pixel_combats/basic';
 import * as DuelTeams from './DuelTeams.js';
 
 // Constants:
-const WaitingPlayersTime = 11;
-const PreparationTime = 73;
+const waiting_players_time = 11;
+const preparationTime = 73;
 const GameModeTime = 271;
 const MockModeTime = 171;
 const EndMatchTime = 16;
@@ -221,63 +221,7 @@ function StartVote() {
     }
 }
 
-function SpawnTeams(Teams) {
- Teams.All.forEach(SpawnTeams => {
-   SpawnTeams.Spawn();
-          } 
- } 
-
-function SetInventoryPreparation() {
- const inventory = Inventory.GetContext();
-  inventory.Main.Value = true;
-  inventory.Secondary.Value = true;
-  inventory.Melee.Value = true;
-  inventory.Explosive.Value = true;
-  inventory.Build.Value = true;
-}
-function SetInventoryGameMode() {
- const inventory = Inventory.GetContext();
-  inventory.Main.Value = false;
- if (GameMode.Parameters.GetBool('MainEnableTrue')) {
-  inventory.Main.Value = true;
- }
-  inventory.Secondary.Value = false;
- if (GameMode.Parameters.GetBool('SecondaryEnableTrue')) {
-  inventory.Secondary.Value = true;
- }
-  inventory.Melee.Value = false;
- if (GameMode.Parameters.GetBool('MeleeEnableTrue')) {
-  inventory.Melee.Value = true;
- }
-  inventory.Explosive.Value = false;
- if (GameMode.Parameters.GetBool('ExplosiveEnableTrue')) {
-  inventory.Explosive.Value = true;
-  inventory.ExplosiveInfinity.Value = true;
- }
-if (GameMode.Parameters.GetBool('BuildEnableTrue')) {
-  inventory.Build.Value = true;
-}
-  inventory.Build.Value = false;
-}
-function SetInventoryWinners(Winners) {
- const inventory = Inventory.GetContext(Winners);
-  inventory.Main.Value = true;
-  inventory.MainInfinity.Value = true;
-  inventory.Secondary.Value = true;
-  inventory.SecondaryInfinity.Value = true;
-  inventory.Melee.Value = true;
-  inventory.Explosive.Value = true;
-  inventory.ExplosiveInfinity.Value = true;
-  inventory.Build.Value = true;
-  inventory.BuildInfinity.Value = true;
-}
-function SetInventoryLoosers(Loosers) {
- const inventory = Inventory.GetContext(Loosers);
-  inventory.Main.Value = false;
-  inventory.Secondary.Value = false;
-  inventory.Melee.Value = false;
-  inventory.Explosive.Value = false;
-  inventory.Build.Value = false;
-}
+library_settings.spawn_teams();
+library_settings.set_inventory_preparation();
 
 ScoresTimer.RestartLoop(TimerInterval_SCORES);
